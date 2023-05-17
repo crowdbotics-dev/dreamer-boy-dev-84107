41c0bca18e1fa19248d6af81398282659063e479
Crowdbotics <engineering@crowdbotics.com>
2023-05-17 21:42:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dreamer-boy-dev-84107",
+  url: "https://dreamer-boy-dev-84107.botics.co",
+  api: "https://dreamer-boy-dev-84107.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
